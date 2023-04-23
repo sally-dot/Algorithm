df21daa7c6ce2d5fa6d1daf3ac276f89ad5acaa2
Sally Lee <75000708+sally-dot@users.noreply.github.com>
2023-04-23 15:39:41 +0900

    [Bronze V] Title: A/B, Time: 160 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+const answer = A/B;
+console.log(answer);

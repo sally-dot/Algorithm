22438116dba95b4cf21ed3c4e28004cd1ef52e05
Sally Lee <75000708+sally-dot@users.noreply.github.com>
2023-05-10 11:36:08 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,6 +1,7 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n').map(Number);
-
-let max = Math.max(...input)
-
-console.log(max)
-console.log(input.indexOf(max) + 1)
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").map(Number)
+const savedInput = [...input]
+const max = input.sort((x,y)=>x-y);
+const maxNum = max[max.length - 1]
+const myIndex = savedInput.indexOf(maxNum) + 1
+console.log(maxNum);
+console.log(myIndex);
